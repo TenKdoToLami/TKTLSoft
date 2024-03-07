e526c3bf10c8876406e92282d25b0ecceab6f819
Jan Bartoš <73753304+TenKdoToLami@users.noreply.github.com>
2024-03-07 22:34:45 +0100

    BitArrayAt implementation blueprint

--- src/TKTL_BitArray.c
+++ src/TKTL_BitArray.c
@@ -1,1 +1,13 @@
 #include "TKTL_BitArray.h"
+
+// Untested
+int BitArrayAt(const struct BitArray * self, unsigned int position)
+{
+  if (position > self->lastElement)
+      return -1;
+  
+  unsigned int byte = position / 8;
+  unsigned int bit = postion % 8;
+  
+  return self->storage[byte] & (1 << bit); 
+}
